de50566b162a504abdcb281e5cb5590bae5fdb2f
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-10-31 23:52:48 -0700

    Test 3 gTest complete

--- Tests/Test1.cpp
+++ Tests/Test1.cpp
@@ -18,17 +18,20 @@ TEST(Test1, CheckPrintingAndAddition)
 {
 	stringstream strs;
 
+	//an actually large #
 	LargeNum num0("1234567890123456789");
 	strs << num0;
 	string ans0Str = "1,234,567,890,123,456,789";
 	EXPECT_EQ(strs.str(), ans0Str);
 	
+	//a smaller #
 	strs.str("");
 	LargeNum num1(12345);
 	strs << num1;
 	string ans1Str = "12,345";
 	EXPECT_EQ(strs.str(), ans1Str);
 
+	// addition w/ smaller #'s
 	strs.str("");
 	LargeNum num2(11115);
 	LargeNum num3 = num1 + num2;
@@ -36,6 +39,7 @@ TEST(Test1, CheckPrintingAndAddition)
 	string ans2Str = "23,460";
 	EXPECT_EQ(strs.str(), ans2Str);
 
+	// another addition w/ smaller #'s
 	strs.str("");
 	LargeNum num4(99);
 	LargeNum num5 = num1 + num4;
@@ -43,6 +47,7 @@ TEST(Test1, CheckPrintingAndAddition)
 	string ans3Str = "12,444";
 	EXPECT_EQ(strs.str(), ans3Str);
 
+	// addition w/ medium #
 	strs.str("");
 	LargeNum num6(99000);
 	LargeNum num7 = num1 + num6;
@@ -50,6 +55,7 @@ TEST(Test1, CheckPrintingAndAddition)
 	string ans4Str = "111,345";
 	EXPECT_EQ(strs.str(), ans4Str);
 
+	// addition w/ large #
 	strs.str("");
 	LargeNum num8 = num0 + num1;
 	strs << num8;
--- Tests/Test3.cpp
+++ Tests/Test3.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Test2
-// Test Desc.  : Test that add + remove work properly for 2 items
+// Test Desc.  : Test to check negative #'s
 //				 	(coverage for old test 3 method)
 // Author      : Jeffrey Caruso
 // Date    	   : Fall 2023
@@ -9,51 +9,60 @@
 #include <gtest/gtest.h>
 #include "applib/largenum.h"
 
+#include <iostream>
+#include <sstream>
+
 using namespace std;
 
-TEST(Test3, LifecycleFor2PokemonEntries)
+TEST(Test3, checkNegativeNumbers)
 {
-	cout << "trivial test for now..." << endl;
-	// //Make Pokedex class pdx
-	// Pokedex pdx;
+	//misc testing x3
+	stringstream strs;
+	LargeNum num0("-1234567890123456789");
+	strs << num0;
+	string ans0Str = "-1,234,567,890,123,456,789";
+	EXPECT_EQ(strs.str(), ans0Str);
 
-	// //string stream for output testing
-	// stringstream strs;
+	strs.str("");
+	LargeNum num1(-12345);
+	strs << num1;
+	string ans1Str = "-12,345";
+	EXPECT_EQ(strs.str(), ans1Str);
 
-	// //insert 2 Pokemon, Pikachu first
-	// pdx.insert("Pikachu");
-	// pdx.insert("Charmander");
+	strs.str("");
+	LargeNum numZero(-0000);
+	strs << numZero;
+	string ans2Str = "0";
+	EXPECT_EQ(strs.str(), ans2Str);
 
-	// //check its size 2 as expected after 2 adds
-	// //yield better error output to user with making it clear 2 is expected result
-	// int expectedResult0 = 2;
-	// EXPECT_EQ(pdx.size(), expectedResult0);
+	//check isZero
+	EXPECT_TRUE(LargeNum(0).isZero());
+	EXPECT_TRUE(LargeNum(-0).isZero());
 
-	// //verifying front and back (2 and only 2 entries present)
-	// EXPECT_EQ(pdx.at(0), pdx.front());
-	// EXPECT_EQ(pdx.at(1), pdx.back());
+	//check == operator on 0, -0.
+	EXPECT_TRUE(LargeNum(0) == LargeNum(-0));
 
-	// //init str stream to empty
-	// strs.str("");
-	// //load output to str stream
-	// strs << pdx;
-	// //string of expected output for comparison
-	// string expectedOutput1 = "[Charmander, Pikachu]";
-	// //check output is expected result
-	// EXPECT_EQ(strs.str(), expectedOutput1);
+	//check negation operator
+	EXPECT_TRUE(LargeNum(0).negate() == LargeNum(-0));
 
-
-	// //now being erasing and verifying that is working as expected...
-	// //erase front (b/c alpha order, delete charamander)
-	// pdx.erase(0);
-	// EXPECT_EQ(pdx.front(), pdx.back());
-
-	// //set str stream to empty
-	// strs.str("");
-	// //load in output
-	// strs << pdx;
-	// //string of expected output for comparison
-	// string expectedOutput2 = "[Pikachu]";
-	// //check output is expected result
-	// EXPECT_EQ(strs.str(), expectedOutput2);
+	//combination add, sub, then equality operator usage...
+	// 100 - 100 = 0
+	EXPECT_TRUE(LargeNum(100) - LargeNum(100) == LargeNum(0));
+	// 100 + -100 = 0
+	EXPECT_TRUE(LargeNum(100) + LargeNum(-100) == LargeNum(0));
+	// 100 - (-100) = 200
+	EXPECT_TRUE(LargeNum(100) - LargeNum(-100) == LargeNum(200));
+	// -100 - (-100) = 0
+	EXPECT_TRUE(LargeNum(-100) - LargeNum(-100) == LargeNum(0));
+	// -100 + 100 = 0
+	EXPECT_TRUE(LargeNum(-100) + LargeNum(100) == LargeNum(0));
+	// 100 - 7 = 93
+	EXPECT_TRUE(LargeNum(100) - LargeNum(7) == LargeNum(93));
+	// 7 - 100 = -93
+	EXPECT_TRUE(LargeNum(7) - LargeNum(100) == LargeNum(-93));
+	// -7 + (-100) = -107
+	EXPECT_TRUE(LargeNum(-7) + LargeNum(-100) == LargeNum(-107));
 }
+
+// There is no meaningful split here that is not already covered by Test 1 or Test 2.
+// Thus, this is best left as 1 comprehensive test of negative numbers and their proper use in student BigNum class
