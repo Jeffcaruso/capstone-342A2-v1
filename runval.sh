9375215cf843f98f79300068499aaad2c2d73b6a
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2024-02-25 03:36:32 +0000

    Final Solution

--- runval.sh
+++ runval.sh
@@ -3,7 +3,7 @@
 rm -f a.out
 
 echo "Compiling: g++ *.cpp -g -lgtest -lgtest_main -pthread"
-g++ *.cpp -lgtest -lgtest_main -pthread
+g++ *.cpp -g -lgtest -lgtest_main -pthread
 
 echo "Executing: valgrind ./a.out"
 valgrind ./a.out
